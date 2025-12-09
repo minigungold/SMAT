86ac2cba29332d6dcc418ee81c9743cb19493361
minigungold <minigungold@gmail.com>
2025-12-09 10:48:29 -0500

    Removed Unused code from PlayingCardSlot

--- Assets/Scripts/PlayingCardSlot.cs
+++ Assets/Scripts/PlayingCardSlot.cs
@@ -2,16 +2,11 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
 
-public class PlayingCardSlot : MonoBehaviour,
-    IPointerEnterHandler, IPointerExitHandler
+public class PlayingCardSlot : MonoBehaviour
 {
 
     public bool isPlayable;
     public bool isOccupied;
-
-    [Header("Events")]
-    [HideInInspector] public UnityEvent<InteractionCarte> PointerEnterEvent;
-    [HideInInspector] public UnityEvent<InteractionCarte> PointerExitEvent;
     public GameObject currentCardObject;
 
 
@@ -25,32 +20,5 @@ public class PlayingCardSlot : MonoBehaviour,
     void Update()
     {
 
-    }
-    public void OnPointerEnter(PointerEventData eventData)
-    {
-        isPlayable = true;
-    }
-
-    public void OnPointerExit(PointerEventData eventData)
-    {
-        isPlayable = false;
-    }
-
-    public void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.GetComponent<InteractionCarte>())
-        {
-            //isOccupied = true;
-            //currentCardObject = collision.gameObject; 
-        }
-    }
-
-    public void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.gameObject.GetComponent<InteractionCarte>())
-        {
-            //isOccupied = false;
-            //currentCardObject = null;
-        }
     }
 }
